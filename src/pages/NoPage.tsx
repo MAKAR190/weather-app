52f50b9b4ba366afd12744e8ebe638ecdde42dd6
MAKAR190 <lutskyimakar@gmail.com>
2024-04-09 17:58:41 +0200

    slight change in a font size

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -22,7 +22,7 @@ const NoPage = ({message}: {message?:string;}) => {
         <div className="App">
             <div className="flex flex-col w-[99%] h-screen justify-center items-center">
                 <img ref={imageRef}  src="/sad-icon.png" alt="sad-icon" />
-                <h1 className="font-bold text-white text-center text-7xl font-sans-['Merriweather']">{message || "404"}</h1>
+                <h1 className="font-bold text-white text-center sm:text-5xl md:text-5xl lg:text-7xl xl:text-7xl xxl:text-7xl font-sans-['Merriweather']">{message || "404"}</h1>
                 <Link to="/">
                   <button className="my-10 text-white bg-black border-blue-400 border-2 rounded-2xl px-5 py-3 duration-[0.1s] hover:border-white">Home</button>
                 </Link>
